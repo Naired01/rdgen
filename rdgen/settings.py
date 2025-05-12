52c3f54cf8e0dc03c594680711189811be43dcc6
Naired <92755796+Naired01@users.noreply.github.com>
2025-05-12 15:27:47 -0700

    Fix path 🔗

--- rdgen/settings.py
+++ rdgen/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'rdgen.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db/db.sqlite3',
+        'NAME': BASE_DIR / 'db\db.sqlite3',
     }
 }
 
